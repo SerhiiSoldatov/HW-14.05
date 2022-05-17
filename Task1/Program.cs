cec6c2154a86e082c3eb43129ba66382a57cae22
Serhii <serhii.soldatov@gmail.com>
2022-05-17 13:22:26 +0200

    task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+int m = 5;
+int n = 5;
+double[,] mass = new double[m, n];
+Random random = new Random();
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = random.NextDouble() * 10;
+        System.Console.Write("{0,6:F1}", mass[i, j]);
+    }
+    System.Console.WriteLine();
+}
